f745ee648136333bb867f4a4ef065aa87932448c
ZoNaDak <kbh3719net@gmail.com>
2018-12-17 13:59:34 +0900

    remove Titie Scene And adding SceneController & Scene

---
+++ Test/Assets/Scripts/Scene/Scene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test.Scene {
+    public abstract class Scene : MonoBehaviour {
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
---
+++ Test/Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test.Scene {
+    public class SceneController : MonoBehaviour {
+        private Scene currentScene;
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+    }
+}
