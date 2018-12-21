49010424d94e6f71b675789dc58b9997f94c86f7
ZoNaDak <kbh3719net@gmail.com>
2018-12-22 00:52:37 +0900

    add LineController Class, Line Prefab

--- Test/Assets/Scripts/Scene/InGame/InGameSceneController.cs
+++ Test/Assets/Scripts/Scene/InGame/InGameSceneController.cs
@@ -2,23 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Test.Bubble;
+using Test.Util;
 
 namespace Test.MyScene {
     public class InGameSceneController : SceneController {
-        public const int BUBBLE_NUM = 42;    
-        public const float GRAVITY_SCALE = 40.0f;
+        public const int CANDY_NUM = 42;    
+        public const float GRAVITY_SCALE = 50.0f;
+
+        [SerializeField] private GameObject levelCanvas;
+        [SerializeField] private GameObject candyCanvas;
+        [SerializeField] private GameObject uiCanvas;
 
         private CandyManager candyManager;
+        private LineController lineForCandy;
 
         void Awake() {
             this.SceneType = ESceneType.InGame;
             Physics2D.gravity = new Vector2(0.0f, -9.81f) * GRAVITY_SCALE;
             this.candyManager = CandyManager.Instance;
             Util.SpriteFactory.Instance.AddAtlas("InGame");
+            //Create Line
+            GameObject linePrefab = PrefabFactory.Instance.FindPrefab("Line");
+            if(linePrefab == null) {
+                linePrefab = PrefabFactory.Instance.CreatePrefab("InGame", "Line", true);
+            }
+            this.lineForCandy = Instantiate(linePrefab, this.candyCanvas.transform).GetComponent<LineController>();
         }
 
         void Start() {
-            this.candyManager.Initialize(BUBBLE_NUM);
+            this.candyManager.Initialize(CANDY_NUM);
+            this.lineForCandy.Initialize(CANDY_NUM);
+            this.lineForCandy.Clear();
+        }
+
+        void Update() {
+        
         }
 
         public void ClickOnStartButton() {
---
+++ Test/Assets/Scripts/Util/Line/LineController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test.Util {
+    public class LineController : MonoBehaviour {
+        const float MY_DEPTH = -2.0f;
+        const float OUTCIRCLE_DEPTH = -1.0f;
+        const float INCIRCLE_DEPTH = -3.0f;
+
+        private LineRenderer myRenderer;
+
+        private Stack<Vector3> points = new Stack<Vector3>();
+        private List<GameObject> outCircles = new List<GameObject>();
+        private List<GameObject> inCircles = new List<GameObject>();
+
+        public int PointNum { get { return this.points.Count; } }
+
+        void Awake() {
+            this.myRenderer = this.gameObject.GetComponent<LineRenderer>();
+        }
+
+        public void Initialize(int _candyNum) {
+            GameObject outCirclePrefab = PrefabFactory.Instance.FindPrefab("OutCircle");
+            if(outCirclePrefab == null) {
+                outCirclePrefab = PrefabFactory.Instance.CreatePrefab("InGame", "OutCircle", true);
+            }
+            GameObject inCirclePrefab = PrefabFactory.Instance.FindPrefab("InCircle");
+            if(inCirclePrefab == null) {
+                inCirclePrefab = PrefabFactory.Instance.CreatePrefab("InGame", "InCircle", true);
+            }
+            
+            for(int i = 0; i < _candyNum; ++i) {
+                this.outCircles.Add(Instantiate(outCirclePrefab, this.transform));
+                this.outCircles[i].SetActive(false);
+                this.inCircles.Add(Instantiate(inCirclePrefab, this.transform));
+                this.inCircles[i].SetActive(false);
+            }
+
+            this.gameObject.SetActive(false);
+        }
+        
+        public void AddPoint(Vector2 _addPoint) {
+            if(this.points.Count == 0) {
+                this.gameObject.SetActive(true);
+            }
+            this.points.Push(new Vector3(_addPoint.x, _addPoint.y, MY_DEPTH));
+
+            this.outCircles[this.points.Count-1].SetActive(true);
+            this.outCircles[this.points.Count-1].transform.localPosition = new Vector3(_addPoint.x, _addPoint.y, OUTCIRCLE_DEPTH);
+            this.inCircles[this.points.Count-1].SetActive(true);
+            this.inCircles[this.points.Count-1].transform.localPosition = new Vector3(_addPoint.x, _addPoint.y, INCIRCLE_DEPTH);
+
+            this.myRenderer.positionCount = this.points.Count;
+            this.myRenderer.SetPositions(this.points.ToArray());
+        }
+
+        public void RemoveLastPoint() {
+            this.outCircles[this.points.Count-1].SetActive(false);
+            this.inCircles[this.points.Count-1].SetActive(false);
+
+            this.points.Pop();
+
+            this.myRenderer.positionCount = this.points.Count;
+            this.myRenderer.SetPositions(this.points.ToArray());
+        }
+
+        public void Clear() {
+            for(int i = 0; i < this.points.Count; ++i) {
+                this.outCircles[i].SetActive(false);
+                this.inCircles[i].SetActive(false);
+            }
+            this.points.Clear();
+            this.myRenderer.positionCount = 0;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
